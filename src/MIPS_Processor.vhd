31ccdbea7a2c913eed0e04ecc5c2eea331d6efb3
eduardoc <eduardoc@co2050-05.ece.iastate.edu>
2021-12-03 12:47:39 -0600

    MIPS_Processor update

--- src/MIPS_Processor.vhd
+++ src/MIPS_Processor.vhd
@@ -16,7 +16,7 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;
--- to do 
+
 entity MIPS_Processor is
   generic(N : integer := 32);
   port(iCLK            : in std_logic;
@@ -36,7 +36,7 @@ architecture structure of MIPS_Processor is
   signal s_DMemAddr     : std_logic_vector(N-1 downto 0); -- TODO: use this signal as the final data memory address input
   signal s_DMemData     : std_logic_vector(N-1 downto 0); -- TODO: use this signal as the final data memory data input
   signal s_DMemOut      : std_logic_vector(N-1 downto 0); -- TODO: use this signal as the data memory output
-
+ 
   -- Required register file signals 
   signal s_RegWr        : std_logic; -- TODO: use this signal as the final active high write enable input to the register file
   signal s_RegWrAddr    : std_logic_vector(4 downto 0); -- TODO: use this signal as the final destination register address input
@@ -52,19 +52,6 @@ architecture structure of MIPS_Processor is
 
   -- Required overflow signal -- for overflow exception detection
   signal s_Ovfl         : std_logic;  -- TODO: this signal indicates an overflow exception would have been initiated
-  component IF_ID is --Instruction Fetch
-	port(iUpdatedPCInstr:	in std_logic_vector(31 downto 0);
-	     iPCAddSrc:		in std_logic_vector(31 downto 0);
-	     iSignExtend:	in std_logic_vector(31 downto 0);
-	     iBranchControl:	in std_logic;
-	     iJumpControl:	in std_logic;
-	     iJALControl:	in std_logic;
-	     overflowPCAdd:	out std_logic;
-	     overflowBranchAdd: out std_logic;
-	     oUpdatedPCAdd:	out std_logic_vector(31 downto 0);
-	     iJALAddr:		out std_logic_vector(31 downto 0)
-	);
-  end component;
 
   component mux2t1_5 -- instruction 20-16 and instruction 15-11
   generic(N : integer := 5); 
@@ -206,6 +193,7 @@ end component;
 
   -- TODO: You may add any additional signals or components your implementation 
   --       requires below this comment
+
 	signal s_regdst, s_jump,s_branch, s_memToReg, s_ALUsrc, s_signExtendControl, s_jr, s_branchSelect, s_jal : std_logic;
 	signal s_InstrMuxOut : std_logic_vector(4 downto 0);
 	signal s_ALUOP, s_ALUcontrolOut : std_logic_vector(3 downto 0);
@@ -231,57 +219,49 @@ end component;
 	signal s_InstrMux1Out : std_logic_vector(4 downto 0);
 	signal s_luiInst : std_logic;
 	signal s_luiMuxOut : std_logic_vector(31 downto 0);
+
 begin
 
-  oALUOut <= s_ALUout;
-  s_DMemAddr <= s_ALUout;
-  s_DMemData <= s_rtOut;
-  
   -- TODO: This is required to be your final input to your instruction memory. This provides a feasible method to externally load the memory module which means that the synthesis tool must assume it knows nothing about the values stored in the instruction memory. If this is not included, much, if not all of the design is optimized out because the synthesis tool will believe the memory to be all zeros.
   with iInstLd select
     s_IMemAddr <= s_NextInstAddr when '0',
       iInstAddr when others;
-  
+
 
   IMem: mem
     generic map(ADDR_WIDTH => 10,
                 DATA_WIDTH => N)
-
-    port map(clk  	=> iCLK,
-             addr 	=> s_IMemAddr(11 downto 2),
-             data 	=> iInstExt,
-             we   	=> iInstLd,
-             q    	=> s_Inst);
+    port map(clk  => iCLK,
+             addr => s_IMemAddr(11 downto 2),
+             data => iInstExt,
+             we   => iInstLd,
+             q    => s_Inst);
   
   DMem: mem
     generic map(ADDR_WIDTH => 10,
                 DATA_WIDTH => N)
-
-    port map(clk  	=> iCLK,
-             addr 	=> s_DMemAddr(11 downto 2),
-             data 	=> s_DMemData,
-             we   	=> s_DMemWr,
-             q    	=> s_DMemOut);
-
+    port map(clk  => iCLK,
+             addr => s_DMemAddr(11 downto 2),
+             data => s_DMemData,
+             we   => s_DMemWr,
+             q    => s_DMemOut);
 
   -- TODO: Ensure that s_Halt is connected to an output control signal produced from decoding the Halt instruction (Opcode: 01 0100)
   -- TODO: Ensure that s_Ovfl is connected to the overflow output of your ALU
 
   -- TODO: Implement the rest of your processor below this comment! 
-	s_Halt <= '1' when s_Inst(31 downto 26) = "010100" 
-	else '0';
-   InstrMux : mux2t1_5
-	generic map (N => 5)
-	port map(i_S => s_regdst,
-		 i_D0 => s_Inst(20 downto 16),
-		 i_D1 => s_Inst(15 downto 11),
-		 o_O => s_InstrMux1Out);
-   InstrMux2 : mux2t1_5
-	generic map (N => 5)
-	port map(i_S => s_jal,
-		 i_D0 => s_InstrMux1Out,
-		 i_D1 => "11111",
-		 o_O => s_RegWrAddr);
+
+--IF
+---------------------------------------------------------------------------
+
+  JrMux : mux2t1_N
+	generic map(N => N)
+	port map(i_S => s_jr,
+		 i_D0 => s_IFIDOut,
+		 i_D1 => s_rsOut,
+		 o_O => s_JRmuxOut32
+	);
+
   g_PC: PCReg
     port MAP( --PC 0x00400000, special register 
 	i_CLK		=> iCLK,
@@ -289,6 +269,21 @@ begin
 	i_WE		=> '1',
 	i_D		=> s_JRmuxOut32,
         o_Q            	=> s_NextInstAddr);
+
+  g_PCADDER: ripplecarryadd_N		--fulladder_n
+    port MAP(
+	i_Ain		=> s_NextInstAddr,
+	i_Bin      	=> x"00000004",	--s_AdderBinSrc
+	i_Cin		=> '0',
+	o_Cout		=> ,
+        o_R            	=> );
+
+
+
+
+--ID
+---------------------------------------------------------------------------
+
   control : controlUnit
 	port map(opcode => s_Inst(31 downto 26),
 		 functionF => s_Inst(5 downto 0),
@@ -305,8 +300,8 @@ begin
 		 ALUOP => s_ALUOP,
 		 i_CLK => iCLK,
 		 luiInst => s_luiInst,
-		 ImmType => s_ImmType
-	);
+		 ImmType => s_ImmType);
+
   ALUcont : ALUcontrol
 	port map(ALUOP 		=> s_ALUOP,
 		 functionF 	=> s_Inst(10 downto 0),
@@ -316,6 +311,76 @@ begin
 		 i_CLK 		=> iCLK,
 		 ImmType	=> s_ImmType
 	);
+
+   InstrMux : mux2t1_5
+	generic map (N => 5)
+	port map(i_S => s_regdst,
+		 i_D0 => s_Inst(20 downto 16),
+		 i_D1 => s_Inst(15 downto 11),
+		 o_O => s_InstrMux1Out);
+   InstrMux2 : mux2t1_5
+	generic map (N => 5)
+	port map(i_S => s_jal,
+		 i_D0 => s_InstrMux1Out,
+		 i_D1 => "11111",
+		 o_O => s_RegWrAddr);
+
+  signExtend : SignExtender 
+	port map(i_S => s_signExtendControl,
+		 i_Extend => s_Inst(15 downto 0),
+		 o_Extended => s_signExtended);
+
+  g_SHIFTL2: shiftleft2
+    port MAP(
+	in32		=> iSignExtend,
+	out32shifted	=> s_shiftedL2Out);
+
+  g_BRANCHADDRADDER: ripplecarryadd_N		--fulladder_n
+    port MAP(
+	i_Ain		=> ,
+	i_Bin      	=> ,	
+	i_Cin		=> '0',
+	o_Cout		=> ,
+        o_R            	=> );
+
+  g_MUX2T1PCSrc: mux2t1_N
+    port MAP(
+	i_S		=> iBranchControl,
+	i_D0      	=> s_PCAdderOut,
+	i_D1		=> s_BranchPC,
+        o_O            	=> s_BranchPCMuxout);
+
+   shiftL26bit : shiftleft226bit
+	port map(in26 => iUpdatedPCInstr(25 downto 0),
+		 out28shifted => s_jumpAdd);
+
+  muxJump : mux2t1_N
+	port map(i_S    => iJumpControl,
+		 i_D0   => s_BranchPCMuxout,
+		 i_D1   => s_FinalJumpAdd,
+		 o_O    => oUpdatedPCAdd
+	);
+
+   InstrMux : mux2t1_5
+	generic map (N => 5)
+	port map(i_S => s_regdst,
+		 i_D0 => s_Inst(20 downto 16),
+		 i_D1 => s_Inst(15 downto 11),
+		 o_O => s_InstrMux1Out);
+   InstrMux2 : mux2t1_5
+	generic map (N => 5)
+	port map(i_S => s_jal,
+		 i_D0 => s_InstrMux1Out,
+		 i_D1 => "11111",
+		 o_O => s_RegWrAddr);
+
+  JrMuxInputReg : mux2t1_5
+	port map(i_S => s_jr,
+		 i_D0 => s_Inst(25 downto 21),
+		 i_D1 => "11111",
+		 o_O => s_jrMuxOut
+	);
+
   RegFile : RegisterFile 
 	generic map(N => N)
 	port map(w_add => s_RegWrAddr,
@@ -326,8 +391,13 @@ begin
 		 i_CLK => iCLK,
 		 rst => iRST,
 		 rs_out => s_rsOut,
-		 rt_out => s_rtOut
-	);
+		 rt_out => s_rtOut);
+
+
+
+--EX
+---------------------------------------------------------------------------
+
   ALUmux : mux2t1_N
 	generic map(N => N)
 	port map(i_S 	=> s_ALUsrc,
@@ -335,6 +405,7 @@ begin
 		 i_D1 	=> s_signExtended,
 		 o_O 	=> s_ALUmuxOut
 	);
+
   MainALU : ALU
 	port map(i_A => s_rsOut,
 	     i_B => s_ALUmuxOut,
@@ -347,18 +418,14 @@ begin
 	     o_carry => s_toNothing,
 	     o_oF => s_Ovfl
 	);
-  andgBranch : andg2
-	port map(i_A 	=> s_branch,
-	     	i_B 	=> s_ALUzero,
-	     	o_F 	=> s_OutAnd
-	);
 
-  signExtend : SignExtender 
-	port map(i_S => s_signExtendControl,
-		 i_Extend => s_Inst(15 downto 0),
-		 o_Extended => s_signExtended
-	);
+--MEM
+---------------------------------------------------------------------------
 
+
+
+--WB
+---------------------------------------------------------------------------
   memToRegMux : mux2t1_N
 	generic map(N => N)
 	port map(i_S => s_memToReg,
@@ -366,12 +433,15 @@ begin
 		 i_D1 => s_DMemOut,
 		 o_O => s_memToRegOut
 	);
-  JrMuxInputReg : mux2t1_5
-	port map(i_S => s_jr,
-		 i_D0 => s_Inst(25 downto 21),
-		 i_D1 => "11111",
-		 o_O => s_jrMuxOut
+
+  luiMux : mux2t1_N
+	generic map(N=>N)
+	port map(i_S => s_luiInst,
+		 i_D0 => s_memToRegOut,
+		 i_D1 => s_Inst(15 downto 0) & x"0000",
+		 o_O => s_luiMuxOut
 	);
+
   JalWrite : mux2t1_N
 	generic map(N => N)
 	port map(i_S => s_jal,
@@ -379,34 +449,6 @@ begin
 		 i_D1 => s_JALAdd,
 		 o_O => s_RegWrData
 	);
-  JrMux : mux2t1_N
-	generic map(N => N)
-	port map(i_S => s_jr,
-		 i_D0 => s_IFIDOut,
-		 i_D1 => s_rsOut,
-		 o_O => s_JRmuxOut32
-	);
-  luiMux : mux2t1_N
-	generic map(N=>N)
-	port map(i_S => s_luiInst,
-		 i_D0 => s_memToRegOut,
-		 i_D1 => s_Inst(15 downto 0) & x"0000",
-		 o_O => s_luiMuxOut
-	);	
-  FetchDecode : IF_ID
-	port map(
-	     iUpdatedPCInstr =>s_Inst,
-	     iPCAddSrc => s_NextInstAddr,
-	     iSignExtend => s_signExtended,
-	     iBranchControl => s_OutAnd,
-	     iJumpControl => s_jump,
-	     overflowPCAdd=> s_toNothing,
-	     overflowBranchAdd => s_toNothing,
-	     oUpdatedPCAdd => s_IFIDOut,
-	     iJALControl => s_jal,
-	     iJALAddr => s_JALAdd
-	);
 
-  
 end structure;
 
