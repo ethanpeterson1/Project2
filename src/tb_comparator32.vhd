6afb532d86c900684ddd47c2ac40dbb573b735b4
jvandrie <jvandrie@vlinux-25.ece.iastate.edu>
2021-12-09 12:02:12 -0600

    updated registers and tb

--- src/tb_comparator32.vhd
+++ src/tb_comparator32.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_textio.all;  -- For logic types I/O
 library std;
-use std.env.all;                -- For hierarchical/external signals
+--use std.env.all;                -- For hierarchical/external signals
 use std.textio.all;             -- For basic I/O-------------------------------------------------------------------------
 
 
